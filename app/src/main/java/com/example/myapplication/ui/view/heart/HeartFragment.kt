ed05a1db83d64cbf0d1d5e2b1bf9142950b53f7f
김재원 <ashwon12@naver.com>
2021-03-04 05:12:49 +0900

    [Add] view- fragment

---
+++ app/src/main/java/com/example/myapplication/ui/view/heart/HeartFragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.ui.view.heart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+class HeartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_heart, container, false)
+    }
+
+}
