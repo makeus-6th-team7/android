78375f2e0b3761341e555d1e49863948137d20c2
김재원 <ashwon12@naver.com>
2021-03-08 01:32:32 +0900

    [Add] tabLayout setting

---
+++ app/src/main/java/com/example/myapplication/ui/adapter/SearchMultiAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+
+class SearchMultiAdapter(
+    private var type: Int,
+    private var ItemsList: ArrayList<String>
+) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private lateinit var view : View
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            1 -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.search_all_item, parent, false)
+                AllViewHolder(view)
+            }
+            2 -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.search_tag_item, parent, false)
+                TagViewHolder(view)
+            }
+            3 -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.search_place_item, parent, false)
+                PlaceViewHolder(view)
+            }
+            else -> throw RuntimeException("알 수 없는 뷰 타입 에러")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ItemsList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(type){
+            1 -> {
+                (holder as AllViewHolder).tv_room_name.text = ItemsList[position]
+            }
+            2 -> {
+                (holder as TagViewHolder).tv_tag_name.text = ItemsList[position]
+            }
+            3 -> {
+
+            }
+        }
+    }
+
+    inner class AllViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv_room_name = itemView.findViewById<TextView>(R.id.tv_room_name)
+    }
+
+    inner class TagViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv_tag_name = itemView.findViewById<TextView>(R.id.tv_search_tag)
+    }
+
+    inner class PlaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/view/search/SearchFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/view/search/SearchFragment.kt
@@ -1,25 +1,80 @@
 package com.example.myapplication.ui.view.search
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TableLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.ui.adapter.SearchMultiAdapter
+import com.example.myapplication.ui.view.filter.FilterActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
     private lateinit var v : View
+    private var allList = ArrayList<String>()
+    private var placeList = ArrayList<String>()
+    private var tagList = ArrayList<String>()
+
+    private lateinit var rv_search : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_search, container, false)
 
-        setTabLayout()
+
+        filterButton()
         return v
     }
 
-    private fun setTabLayout() {
+    private fun filterButton() {
+        val ib_filter = v.findViewById<ImageButton>(R.id.ib_filter)
+        ib_filter.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, FilterActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun TabLayout.addOnTabSelectedListener() {
+        when(selectedTabPosition){
+            0 -> {
+                allList.clear()
+                allList.add("강릉 네이처")
+                allList.add("강릉 굿플레이스")
+                allList.add("강릉 새벽정원")
+                rv_search = v.findViewById(R.id.rv_search_result)
+                rv_search.adapter = SearchMultiAdapter(1,allList)
+                Log.d("여기는 탭 응답하라 오버","${selectedTabPosition}이 응답했다 오바")
+            }
+            1 -> {
+                allList.clear()
+                allList.add("강릉 네이처")
+                allList.add("강릉 굿플레이스")
+                allList.add("강릉 새벽정원")
+                rv_search = v.findViewById(R.id.rv_search_result)
+                rv_search.adapter = SearchMultiAdapter(2,allList)
+                Log.d("여기는 탭 응답하라 오버","${selectedTabPosition}이 응답했다 오바")
+
+            }
+            3 -> {
+
+            }
+        }
     }
 
 }
+
+private fun TabLayout.setOnTabSelectedListener() {
+    TODO("Not yet implemented")
+}
+
